52c3f472f45a33f13ec4c25666ed0c54b34eb542
unknown <orf@OR-LAPTOP.dm.cx>
2015-06-02 19:32:20 +0300

    new origin commit

---
+++ data.framework/HacmeDB.bat
@@ -0,0 +1,8 @@
+echo off
+cls
+echo HACME DATABASE INSTRUCTIONS
+echo 1 - Type:   "HSQL Database Engine Standalone"
+echo 2 - Recent: "HacmeDB_Standalone"
+echo 3 - Click "OK"
+pause
+java -cp C:\JavaDev\HacmeBooks\lib\hsqldb\hsqldb.jar org.hsqldb.util.DatabaseManager
